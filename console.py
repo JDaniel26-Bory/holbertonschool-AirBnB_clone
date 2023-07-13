b214d1bf8ca0fb7ffd1fd5c6ac6f673e84cc39ee
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-07-13 15:38:57 -0500

    update the file console.py

--- console.py
+++ console.py
@@ -2,12 +2,16 @@
 """Entry point of the command interpreter."""
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class."""
 
     prompt = "(hbnb) "
+
+    valid_classes = {"BaseModel"}
 
     def do_quit(self, arg):
         """Exit the program."""
@@ -25,6 +29,137 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing when an empty line is entered."""
         pass
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel"""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        class_name = args[0]
+
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+
+        new_instance = eval(f"{class_name}()")  # Create new instance
+        new_instance.save()  # Save the instance
+        print(new_instance.id)  # Print the ID
+
+    def do_show(self, arg):
+        """Print the string representation of an instance"""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        class_name = args[0]
+
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+        key = f"{class_name}.{instance_id}"
+        all_objects = storage.all()
+
+        if key in all_objects:
+            print(all_objects[key])
+        else:
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Delete an instance based on the class name and id"""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        class_name = args[0]
+
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+        key = f"{class_name}.{instance_id}"
+        all_objects = storage.all()
+
+        if key in all_objects:
+            del all_objects[key]
+            storage.save()
+        else:
+            print("** no instance found **")
+
+    def do_all(self, arg):
+        """Print all instances"""
+        args = arg.split()
+        all_objects = storage.all()
+
+        if not arg:
+            print([str(obj) for obj in all_objects.values()])
+            return
+
+        class_name = args[0]
+
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+
+        filtered_objects = [
+            str(obj) for obj in all_objects.values()
+            if obj.__class__.__name__ == class_name
+        ]
+        print(filtered_objects)
+
+    def do_update(self, arg):
+        """Update an instance"""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        class_name = args[0]
+
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+        key = f"{class_name}.{instance_id}"
+        all_objects = storage.all()
+
+        if key not in all_objects:
+            print("** no instance found **")
+            return
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+
+        attribute_name = args[2]
+
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        attribute_value = args[3]
+        instance = all_objects[key]
+        setattr(instance, attribute_name, attribute_value)
+        instance.save()
 
 
 if __name__ == "__main__":
